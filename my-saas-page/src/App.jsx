8aef5ec57a36352bc27bde842a2dc6e5d8cc3a0f
yatsilmatt <yatsilmatt@gmail.com>
2024-11-06 13:58:27 +0200

    first commit

---
+++ my-saas-page/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './sections/Header'
+
+
+const App = () => {
+  return (
+    <main className='overflow-hidden'>
+      <Header/>
+    </main>
+  )
+}
+
+export default App
---
+++ my-saas-page/src/sections/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className='fixed top-0 left-0 z-50 w-full py-10'>
+        <div className='container flex h-14 items-center max-lg:px-5'>
+            <a className='lg:hidden flex-1 cursor-pointer z-2'>
+                <img src="/images/xora.svg" width={115} height={55} alt='logo'/>
+            </a>
+            <div className='w-full border-2 border-amber-400'>
+                <nav>
+                    <ul className='flex max-lg:block max-lg:px-12'>
+                        <li className='nav-il'>
+
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
